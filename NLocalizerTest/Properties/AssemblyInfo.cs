b16c64eb8b54a38a4cc4b5c7c10c9c9947007064
Ravi Patel <ravi15@outlook.com>
2018-03-20 11:48:58 +0530

    Added appveyor.yml.

--- NLocalizerTest/Properties/AssemblyInfo.cs
+++ NLocalizerTest/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RBSoft")]
 [assembly: AssemblyProduct("NLocalizerTest")]
-[assembly: AssemblyCopyright("Copyright © 2017 RBSoft")]
+[assembly: AssemblyCopyright("Copyright © 2018 RBSoft")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
